99ba5b518b8e4a875699d6097a45ca54a170d850
omarfarukee <umarfaruque1920@gmail.com>
2024-04-18 16:07:37 +0600

    home preloader animation implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import MainLayout from './components/layout/MainLayout'
+import PreLoader from './components/preLoader/PreLoader'
 
 function App() {
 
   return (
     <>
+    <PreLoader/>
     <MainLayout />
    </>
   )
