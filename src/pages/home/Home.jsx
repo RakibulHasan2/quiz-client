4ba490f1e4e47080107792e102e0ebabdaf1243b
omarfarukee <umarfaruque1920@gmail.com>
2024-04-22 14:10:45 +0600

    home imgage implemet

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,7 @@
 import Footer from "../footer/Footer";
 import './home.css'
+import man from '../../images/cartoon image/360_F_458630388_h2oAUzpYozE7lmpswiot0DPfy7ptYufJ-removebg-preview.png'
+import paper from '../../images/gif/wired-lineal-56-document.gif'
 import 'animate.css';
 const Home = () => {
     return (
@@ -13,8 +15,13 @@ const Home = () => {
                 <div className="circle medium shade4"></div>
                 <div className="circle small shade5"></div>
             </div>
-          <div className="flex justify-center">
-            <p className="animate__animated animate__rubberBand">this is home man</p>
+          <div className="flex justify-between border">
+                <div>
+                    <img className="w-12" src={paper} alt="" />
+                </div>
+            <div className="flex justify-end border">
+                <img className="animate__animated animate__bounceInRight" src={man} alt="" />
+            </div>
             </div>  
           
         </div>
