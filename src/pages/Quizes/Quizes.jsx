61dc442d433ffe95f8921bf578b62d93bc132138
omarfarukee <umarfaruque1920@gmail.com>
2024-05-10 09:47:00 +0600

    api intregation in card page done

--- src/pages/Quizes/Quizes.jsx
+++ src/pages/Quizes/Quizes.jsx
@@ -15,12 +15,12 @@ const Quizes = () => {
     const { data: allCategory = [] } = useQuery({
         queryKey: ['allCategory'],
         queryFn: async () => {
-            const res = await fetch(`https://localhost:7274/api/Question/Fetch`);
+            const res = await fetch(`http://localhost:5000/api/Question/Fetch`);
             const data = await res.json();
             return data;
         }
     });
-    const uniqueCategoryNames = new Set(allCategory?.map(category => category?.questionCategory));
+    const uniqueCategoryNames = new Set(allCategory?.data?.map(category => category?.questionCategory));
     console.log(allCategory)
     return (
         <div className="pt-24 quiz-container">
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -36,7 +36,7 @@ const routes = createBrowserRouter([
                 path: '/quizPage/:getByCatName',
                 element: <QuizOptionPage/>,
                 loader:async ({questionCategory}) =>{
-                    return fetch(`https://localhost:7274/api/Question/getByCatName/${questionCategory}`)
+                    return fetch(`http://localhost:5000/api/Question/getByCatName/${questionCategory}`)
                 }
             },
         ],
